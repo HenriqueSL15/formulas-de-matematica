0a8d0d575edb2f98eddd3aefa0994bd022731488
Henrique Siqueira Lima <henrynyyls@gmail.com>
2024-07-05 14:52:41 -0300

    I added images to help on each formula

--- script.js
+++ script.js
@@ -3,62 +3,70 @@ const questions = [
     question: 'Área do Retângulo',
     answer: ['A', 'b', 'x', 'h', '?', '?', '?'],
     type: 4,
-    img: '',
+    img: 'img/retangulo.png',
   },
   {
     question: 'Área do Paralelogramo',
     answer: ['A', 'b', 'x', 'h', '?', '?', '?'],
     type: 4,
+    img: 'img/paralelogramo.png',
   },
   {
     question: 'Área do Círculo',
     answer: ['A', 'pi', 'x', 'r2', '?', '?', '?'],
     type: 4,
+    img: 'img/circulo.png',
   },
   {
     question: 'Área do Triângulo',
     answer: ['A', 'b', 'x', 'h', '?', '?', '2'],
     type: 5,
+    img: 'img/triangulo.png',
   },
   {
     question: 'Área do Quadrado',
     answer: ['A', 'l2', '?', '?', '?', '?', '?'],
     type: 2,
+    img: 'img/quadrado.png',
   },
   {
     question: 'Área do Losango',
     answer: ['A', 'D', 'x', 'd', '?', '?', '2'],
     type: 4,
+    img: 'img/losango.png',
   },
   {
     question: 'Volume do Cubo',
     answer: ['V', 'a3', '?', '?', '?', '?', '?'],
     type: 4,
+    img: 'img/cubo.png',
   },
   {
     question: 'Volume do Paralelepípedo',
     answer: ['V', 'a', 'x', 'b', 'x', 'c', '?'],
     type: 6,
+    img: 'img/paralelepipedo.png',
   },
   {
     question: 'Volume do Cilindro',
     answer: ['V', 'pi', 'x', 'r2', 'x', 'h', '?'],
     type: 6,
+    img: 'img/cilindro.png',
   },
   {
     question: 'Volume do Cone',
     answer: ['V', '1/3xpi', 'x', 'r2', 'x', 'h', '?'],
     type: 6,
-  },
-  {
-    question: 'Volume da Esfera',
-    answer: ['V', '4/3xpi', 'x', 'r3', '?', '?', '?'],
-    type: 6,
+    img: 'img/cone.png',
   },
 ];
 
+const image = document.getElementById('image');
+
 let questionNumber = getRandomInt(0, questions.length);
+// questionNumber = 4;
 question.innerText = questions[questionNumber].question;
+image.setAttribute('src', `${questions[questionNumber].img}`);
 
 let points = 0;
 
@@ -66,21 +74,17 @@ function verifyValues(array, typeOfQuestion) {
   let i = 0;
 
   array.forEach((element) => {
-    console.log(element, typeOfQuestion.answer[i]);
     if (element != typeOfQuestion.answer[i]) {
-      console.log('errado');
     } else {
-      console.log('correto');
       i++;
     }
-    console.log(i);
   });
 
   if (i == typeOfQuestion.answer.length) {
     questionNumber = getRandomInt(0, questions.length);
     question.innerText = questions[questionNumber].question;
+    image.setAttribute('src', `${questions[questionNumber].img}`);
     points++;
-    console.log(points);
     clearInputs();
     return true;
   } else {
@@ -119,40 +123,8 @@ function submitForm() {
 
   const preview = document.getElementById('preview');
 
-  if (question.innerText == questions[0].question) {
-    verifyValues(
-      [option1, option2, operator1, option3, operator2, option4, option5],
-      questions[questionNumber]
-    );
-  } else if (question.innerText == questions[1].question) {
-    verifyValues(
-      [option1, option2, operator1, option3, operator2, option4, option5],
-      questions[questionNumber]
-    );
-  } else if (question.innerText == questions[2].question) {
-    verifyValues(
-      [option1, option2, operator1, option3, operator2, option4, option5],
-      questions[questionNumber]
-    );
-  } else if (question.innerText == questions[3].question) {
-    verifyValues(
-      [option1, option2, operator1, option3, operator2, option4, option5],
-      questions[questionNumber]
-    );
-  } else if (question.innerText == questions[4].question) {
-    verifyValues(
-      [option1, option2, operator1, option3, operator2, option4, option5],
-      questions[questionNumber]
-    );
-  } else if (question.innerText == questions[5].question) {
-    verifyValues(
-      [option1, option2, operator1, option3, operator2, option4, option5],
-      questions[questionNumber]
-    );
-  } else if (question.innerText == questions[6].question) {
-    verifyValues(
-      [option1, option2, operator1, option3, operator2, option4, option5],
-      questions[questionNumber]
-    );
-  }
+  verifyValues(
+    [option1, option2, operator1, option3, operator2, option4, option5],
+    questions[questionNumber]
+  );
 }
