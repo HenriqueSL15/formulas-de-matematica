c4b058985c6ab169ed983a58a601139272b73cc5
Henrique Siqueira Lima <henrynyyls@gmail.com>
2024-07-04 21:25:27 -0300

    Added more formulas and answers

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const questions = [
     question: 'Área do Retângulo',
     answer: ['A', 'b', 'x', 'h', '?', '?', '?'],
     type: 4,
+    img: '',
   },
   {
     question: 'Área do Paralelogramo',
@@ -17,12 +18,42 @@ const questions = [
   {
     question: 'Área do Triângulo',
     answer: ['A', 'b', 'x', 'h', '?', '?', '2'],
-    type: 4,
+    type: 5,
   },
   {
     question: 'Área do Quadrado',
     answer: ['A', 'l2', '?', '?', '?', '?', '?'],
+    type: 2,
+  },
+  {
+    question: 'Área do Losango',
+    answer: ['A', 'D', 'x', 'd', '?', '?', '2'],
     type: 4,
+  },
+  {
+    question: 'Volume do Cubo',
+    answer: ['V', 'a3', '?', '?', '?', '?', '?'],
+    type: 4,
+  },
+  {
+    question: 'Volume do Paralelepípedo',
+    answer: ['V', 'a', 'x', 'b', 'x', 'c', '?'],
+    type: 6,
+  },
+  {
+    question: 'Volume do Cilindro',
+    answer: ['V', 'pi', 'x', 'r2', 'x', 'h', '?'],
+    type: 6,
+  },
+  {
+    question: 'Volume do Cone',
+    answer: ['V', '1/3xpi', 'x', 'r2', 'x', 'h', '?'],
+    type: 6,
+  },
+  {
+    question: 'Volume da Esfera',
+    answer: ['V', '4/3xpi', 'x', 'r3', '?', '?', '?'],
+    type: 6,
   },
 ];
 
@@ -35,14 +66,14 @@ function verifyValues(array, typeOfQuestion) {
   let i = 0;
 
   array.forEach((element) => {
-    // console.log(element, typeOfQuestion.answer[i]);
+    console.log(element, typeOfQuestion.answer[i]);
     if (element != typeOfQuestion.answer[i]) {
       console.log('errado');
     } else {
       console.log('correto');
       i++;
     }
-    // console.log(i);
+    console.log(i);
   });
 
   if (i == typeOfQuestion.answer.length) {
@@ -50,10 +81,22 @@ function verifyValues(array, typeOfQuestion) {
     question.innerText = questions[questionNumber].question;
     points++;
     console.log(points);
+    clearInputs();
     return true;
   } else {
     return false;
   }
+}
+
+function clearInputs() {
+  document.getElementById('first-part').value = '?';
+  document.getElementById('second-part').value = '?';
+  document.getElementById('third-part').value = '?';
+  document.getElementById('fourth-part').value = '?';
+  document.getElementById('fifth-part').value = '?';
+
+  document.getElementById('first-operator').value = '?';
+  document.getElementById('second-operator').value = '?';
 }
 
 function getRandomInt(min, max) {
@@ -97,6 +140,16 @@ function submitForm() {
       questions[questionNumber]
     );
   } else if (question.innerText == questions[4].question) {
+    verifyValues(
+      [option1, option2, operator1, option3, operator2, option4, option5],
+      questions[questionNumber]
+    );
+  } else if (question.innerText == questions[5].question) {
+    verifyValues(
+      [option1, option2, operator1, option3, operator2, option4, option5],
+      questions[questionNumber]
+    );
+  } else if (question.innerText == questions[6].question) {
     verifyValues(
       [option1, option2, operator1, option3, operator2, option4, option5],
       questions[questionNumber]
